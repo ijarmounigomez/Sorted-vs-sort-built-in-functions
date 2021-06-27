ba46059b0e3cfb462c0025f9aef5ac9556b0c75c
Ismael Jarmouni Gómez <ijarmounigomez@gmail.com>
2021-06-28 00:16:21 +0200

    Update sorted_vs_sort.py

--- sorted_vs_sort.py
+++ sorted_vs_sort.py
@@ -1,3 +1,15 @@
 #Sorted built-in function
 
-list1 = []
+semester_grades1 = [7.5,6.4,4.2,9.5,6.7,3.5,2,4,7.6,5.7]
+
+sorted_semester_grades1 = sorted(semester_grades1)
+
+print(sorted_semester_grades1)
+
+#Sort built-in function
+
+semester_grades2 = [7.5,6.4,4.2,9.5,6.7,3.5,2,4,7.6,5.7]
+
+sorted_semester_grades2 = semester_grades2.sort()
+
+print(sorted_semester_grades2)
